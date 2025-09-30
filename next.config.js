d8821eaeae4bb39ac6a7457a3dd7d2fa57c8f431
ameeralns <ameeralns35@gmail.com>
2025-09-30 17:31:47 -0500

    proo

--- next.config.js
+++ next.config.js
@@ -2,10 +2,26 @@
 const nextConfig = {
   images: {
     domains: [
-      'lh3.googleusercontent.com', // Google OAuth profile pictures
-      'firebasestorage.googleapis.com', // Firebase Storage
+      'lh3.googleusercontent.com',
+      'firebasestorage.googleapis.com',
+    ],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '*.amazonaws.com',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
-}
+  webpack: (config) => {
+    const path = require('path');
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      '@': path.resolve(__dirname, 'src'),
+    };
+    return config;
+  },
+};
 
-module.exports = nextConfig 
+module.exports = nextConfig;
